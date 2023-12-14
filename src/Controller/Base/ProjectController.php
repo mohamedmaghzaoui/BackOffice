3fe31e3abdd1cc1b8ddb440118896d3849f483c1
Mohamed Maghzaoui <mohamed.maghzaoui53@gmail.com>
2023-12-14 16:14:10 +0100

    added initial project controller and twig but there is more work for back office after

---
+++ src/Controller/Base/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Base;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectController extends AbstractController
+{
+    #[Route('/project', name: 'app_project')]
+    public function index(): Response
+    {
+        return $this->render('project/project.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+}
